8e8ebd1d9468c9a75a12087147569443cefdb672
anthony-th <yefremov.anthony@gmail.com>
2023-11-27 20:50:07 +0300

    feat: add footer-right (Mon, Nov 27, 2023 08:50:07 PM)

--- coffee-house/src/components/footer.js
+++ coffee-house/src/components/footer.js
@@ -5,8 +5,6 @@ leftBlock.classList.add('footer-block', 'left-block');
 const leftTitle = document.createElement('h2');
 leftTitle.classList.add('left-title');
 leftTitle.innerHTML = `Sip, Savor, Smile. <span class="italic-accent">It’s coffee time!</span>`
-const leftRight = document.createElement('div');
-leftRight.classList.add('footer-block', 'right-block');
 const socialLinks = document.createElement('div');
 socialLinks.classList.add('social-block');
 const tw = document.createElement('a');
@@ -32,7 +30,61 @@ face.target = '_blank';
 face.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
 <path d="M17 2H14C12.6739 2 11.4021 2.52678 10.4645 3.46447C9.52678 4.40215 9 5.67392 9 7V10H6V14H9V22H13V14H16L17 10H13V7C13 6.73478 13.1054 6.48043 13.2929 6.29289C13.4804 6.10536 13.7348 6 14 6H17V2Z" stroke="#E1D4C9" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>`;
+const rightBlock = document.createElement('div');
+rightBlock.classList.add('footer-block', 'right-block');
+const rightTitle = document.createElement('h2');
+rightTitle.classList.add('right-title');
+rightTitle.textContent = 'Contact us';
+const rightContacts = document.createElement('ul');
+rightContacts.classList.add('contacts-list');
+
+const address = document.createElement('li');
+address.classList.add('contacts-link');
+const iconAddress = document.createElement('svg');
+iconAddress.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+<path d="M16.6663 8.33329C16.6663 12.0152 9.99967 18.3333 9.99967 18.3333C9.99967 18.3333 3.33301 12.0152 3.33301 8.33329C3.33301 4.65139 6.31778 1.66663 9.99967 1.66663C13.6816 1.66663 16.6663 4.65139 16.6663 8.33329Z" stroke="#E1D4C9" stroke-width="1.5"/>
+<path d="M10.0003 9.16667C10.4606 9.16667 10.8337 8.79357 10.8337 8.33333C10.8337 7.8731 10.4606 7.5 10.0003 7.5C9.54009 7.5 9.16699 7.8731 9.16699 8.33333C9.16699 8.79357 9.54009 9.16667 10.0003 9.16667Z" fill="#E1D4C9" stroke="#E1D4C9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>`;
+const linkAddress = document.createElement('a');
+linkAddress.classList.add('address-link');
+linkAddress.textContent = `8558 Green Rd.,  LA`;
+linkAddress.href = 'https://www.google.com/maps?q=8558+Green+Rd.,+LA';
+linkAddress.target = '_blank';
+
+const tel = document.createElement('li');
+tel.classList.add('contacts-link');
+const iconTel = document.createElement('svg');
+iconTel.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+<path d="M15.0984 12.2516L11.6665 12.9166C9.34845 11.7531 7.91654 10.4166 7.08321 8.33329L7.72483 4.89154L6.51197 1.66663L3.72946 1.66663C2.60191 1.66663 1.71466 2.59958 1.90108 3.71161C2.29888 6.08454 3.37231 10.0391 6.24987 12.9166C9.27338 15.9401 13.5661 17.3318 16.1378 17.9288C17.299 18.1983 18.3332 17.2908 18.3332 16.0988L18.3332 13.4843L15.0984 12.2516Z" stroke="#E1D4C9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>`;
+const linkTel = document.createElement('a');
+linkTel.classList.add('tel-link');
+linkTel.textContent = `+1 (603) 555-0123`;
+linkTel.href = 'tel:+16035550123';
+
+const timeToWork = document.createElement('li');
+timeToWork.classList.add('contacts-link');
+const iconTime = document.createElement('svg');
+iconTime.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+<g clip-path="url(#clip0_217_1736)">
+  <path d="M10 5L10 10L15 10" stroke="#E1D4C9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M10.0003 18.3333C14.6027 18.3333 18.3337 14.6023 18.3337 9.99996C18.3337 5.39759 14.6027 1.66663 10.0003 1.66663C5.39795 1.66663 1.66699 5.39759 1.66699 9.99996C1.66699 14.6023 5.39795 18.3333 10.0003 18.3333Z" stroke="#E1D4C9" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</g>
+<defs>
+  <clipPath id="clip0_217_1736">
+    <rect width="20" height="20" fill="white"/>
+  </clipPath>
+</defs>
+</svg>`;
+const linkTime = document.createElement('a');
+linkTime.classList.add('time-link');
+linkTime.textContent = `Mon-Sat: 9:00 AM – 23:00 PM`;
 
 socialLinks.append(tw, ins, face);
 leftBlock.append(leftTitle, socialLinks);
-footer.append(leftBlock, leftRight);
+address.append(iconAddress, linkAddress);
+tel.append(iconTel, linkTel);
+timeToWork.append(iconTime, linkTime);
+rightContacts.append(address, tel, timeToWork);
+rightBlock.append(rightTitle, rightContacts);
+footer.append(leftBlock, rightBlock);
