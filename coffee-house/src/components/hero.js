7b59df7062f5dd682abfea85a42a0b541d42d60d
anthony-th <yefremov.anthony@gmail.com>
2023-11-27 11:28:26 +0300

    feat: add hero section (Mon, Nov 27, 2023 11:28:25 AM)

---
+++ coffee-house/src/components/hero.js
@@ -0,0 +1,17 @@
+export const heroSection = document.createElement('section');
+heroSection.classList.add('section', 'hero-section');
+const heroContent = document.createElement('div');
+heroContent.classList.add('hero-block');
+const heroTitle = document.createElement('h1');
+heroTitle.classList.add('hero-title');
+heroTitle.innerHTML = `<span>Enjoy</span> premium coffee at our charming cafe`
+const heroSubtitle = document.createElement('p');
+heroSubtitle.textContent = 'With its inviting atmosphere and delicious coffee options, the Coffee House Resource is a popular destination for coffee lovers and those seeking a warm and inviting space to enjoy their favorite beverage.';
+heroSubtitle.classList.add('hero-text');
+const heroButton = document.createElement('a');
+heroButton.classList.add('hero-btn', 'cursor-pointer');
+heroButton.href = './menu';
+heroButton.innerHTML = '<span>Menu</span>';
+
+heroContent.append(heroTitle, heroSubtitle, heroButton);
+heroSection.append(heroContent);
--- coffee-house/src/components/main.js
+++ coffee-house/src/components/main.js
@@ -1,2 +1,6 @@
+import { heroSection } from "./hero";
+
 export const main = document.createElement('main');
 main.classList.add('main');
+
+main.append(heroSection);
