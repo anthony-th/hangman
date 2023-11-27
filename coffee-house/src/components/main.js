1c20cfd131e25ce9c990fe8f1a61a29e9fe92f49
anthony-th <yefremov.anthony@gmail.com>
2023-11-28 00:59:36 +0300

    feat: add mobile-app section (Tue, Nov 28, 2023 12:59:35 AM)

--- coffee-house/src/components/main.js
+++ coffee-house/src/components/main.js
@@ -1,8 +1,9 @@
 import { heroSection } from "./hero";
 import { favoriteSection } from "./favorite";
 import { aboutSection } from "./about";
+import { mobileSection } from "./mobileapp";
 
 export const main = document.createElement('main');
 main.classList.add('main');
 
-main.append(heroSection, favoriteSection, aboutSection);
+main.append(heroSection, favoriteSection, aboutSection, mobileSection);
