b68b9087f002e1bc4d494647a1847de04bc72aa3
anthony-th <yefremov.anthony@gmail.com>
2023-12-12 04:27:23 +0300

    feat: add desktop modal (Tue, Dec 12, 2023 04:27:22 AM)

--- coffee-house/src/components/menu.js
+++ coffee-house/src/components/menu.js
@@ -88,6 +88,10 @@ function filterProducts(category) {
     menuListItem.append(listImgWrapper, listTextWrapper);
     manuList.append(menuListItem);
 
+    menuListItem.onclick = function() {
+      openModal(item);
+    };
+
     cardVisible.push(menuListItem);
   });
 }
@@ -171,6 +175,167 @@ filterProducts('coffee');
 checkInnerWidthForTabReload();
 
 window.onresize = resizeWin;
+
+function openModal(item) {
+  const shadow = document.createElement("div");
+  shadow.classList.add("modal-shadow");
+
+  const modal = document.createElement("div");
+  modal.classList.add("modal");
+  const blockImage = document.createElement("div");
+  blockImage.classList.add("modal-image-block");
+  const modalImage = document.createElement("img");
+  modalImage.classList.add("modal-image");
+  modalImage.src = item.img;
+
+  const blockText = document.createElement("div");
+  blockText.classList.add("modal-text-block");
+  const titleBlock = document.createElement("div");
+  titleBlock.classList.add("title-block");
+  const title = document.createElement("h2");
+  title.classList.add("title-modal");
+  title.textContent = item.name;
+  const subTitle = document.createElement("p");
+  subTitle.classList.add("modal-description");
+  subTitle.textContent = item.description;
+  const sizeBlock = document.createElement("div");
+  sizeBlock.classList.add("size-block");
+  const sizeTitle = document.createElement("p");
+  sizeTitle.classList.add("size-title");
+  sizeTitle.textContent = 'Size';
+  const sizeButtons = document.createElement("div");
+  sizeButtons.classList.add("size-buttons");
+
+  const sizeButton1 = document.createElement("div");
+  sizeButton1.classList.add("size-button");
+  const sizeCircle1 = document.createElement("div");
+  sizeCircle1.classList.add("size-circle");
+  sizeCircle1.textContent = 'S';
+  const sizeSml = document.createElement("p");
+  sizeSml.classList.add('size-ml');
+  sizeSml.textContent = item.sizes.s.size;
+
+  const sizeButton2 = document.createElement("div");
+  sizeButton2.classList.add("size-button");
+  const sizeCircle2 = document.createElement("div");
+  sizeCircle2.classList.add("size-circle");
+  sizeCircle2.textContent = 'M';
+  const sizeMml = document.createElement("p");
+  sizeMml.classList.add('size-ml');
+  sizeMml.textContent = item.sizes.m.size;
+
+  const sizeButton3 = document.createElement("div");
+  sizeButton3.classList.add("size-button");
+  const sizeCircle3 = document.createElement("div");
+  sizeCircle3.classList.add("size-circle");
+  sizeCircle3.textContent = 'L';
+  const sizeLml = document.createElement("p");
+  sizeLml.classList.add('size-ml');
+  sizeLml.textContent = item.sizes.l.size;
+
+  const additives = document.createElement("div");
+  additives.classList.add("additives");
+  const additivesTitle = document.createElement("p");
+  additivesTitle.classList.add("additives-title");
+  additivesTitle.textContent = 'Additives';
+
+  const additivesButtons = document.createElement("div");
+  additivesButtons.classList.add("additives-buttons");
+
+  const additivesButton1 = document.createElement("div");
+  additivesButton1.classList.add("additives-button");
+  const additivesCircle1 = document.createElement("div");
+  additivesCircle1.classList.add("additives-circle");
+  additivesCircle1.textContent = '1';
+  const additivesSugar = document.createElement("p");
+  additivesSugar.classList.add('additives-elements');
+  additivesSugar.textContent = item.additives[0].name;
+
+  const additivesButton2 = document.createElement("div");
+  additivesButton2.classList.add("additives-button");
+  const additivesCircle2 = document.createElement("div");
+  additivesCircle2.classList.add("additives-circle");
+  additivesCircle2.textContent = '2';
+  const additivesCin = document.createElement("p");
+  additivesCin.classList.add('additives-elements');
+  additivesCin.textContent = item.additives[1].name;
+
+  const additivesButton3 = document.createElement("div");
+  additivesButton3.classList.add("additives-button");
+  const additivesCircle3 = document.createElement("div");
+  additivesCircle3.classList.add("additives-circle");
+  additivesCircle3.textContent = '3';
+  const additivesSy = document.createElement("p");
+  additivesSy.classList.add('additives-elements');
+  additivesSy.textContent = item.additives[2].name;
+
+  document.body.classList.add('overflow-hidden');
+
+  const totalBlock = document.createElement("div");
+  totalBlock.classList.add("total-block");
+  const totalText = document.createElement("p");
+  totalText.classList.add("total-text");
+  totalText.textContent = 'Total:';
+  const totalPrice = document.createElement("p");
+  totalPrice.classList.add("total-price");
+  totalPrice.textContent = `$${item.price}`
+
+  const alertBlock = document.createElement("div");
+  alertBlock.classList.add("alert-block");
+  const alertSvg = document.createElement("svg");
+  alertSvg.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+  <g clip-path="url(#clip0_268_12877)">
+    <path d="M8 7.66663V11" stroke="#403F3D" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M8 5.00667L8.00667 4.99926" stroke="#403F3D" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M7.99967 14.6667C11.6816 14.6667 14.6663 11.6819 14.6663 8.00004C14.6663 4.31814 11.6816 1.33337 7.99967 1.33337C4.31778 1.33337 1.33301 4.31814 1.33301 8.00004C1.33301 11.6819 4.31778 14.6667 7.99967 14.6667Z" stroke="#403F3D" stroke-linecap="round" stroke-linejoin="round"/>
+  </g>
+  <defs>
+    <clipPath id="clip0_268_12877">
+      <rect width="16" height="16" fill="white"/>
+    </clipPath>
+  </defs>
+  </svg>`;
+  const alertTitle = document.createElement("p");
+  alertTitle.classList.add("alert-title");
+  alertTitle.textContent = 'The cost is not final. Download our mobile app to see the final price and place your order. Earn loyalty points and enjoy your favorite coffee with up to 20% discount.';
+
+  const closeButton = document.createElement("button");
+  closeButton.classList.add("button-close");
+  closeButton.textContent = 'Close';
+  closeButton.onclick = function() {
+    closeModal();
+  };
+
+  function closeModal() {
+    modal.remove();
+    shadow.remove();
+    document.body.classList.remove('overflow-hidden');
+  }
+
+  additivesButtons.append(additivesButton1, additivesButton2, additivesButton3);
+  additivesButton1.append(additivesCircle1, additivesSugar);
+  additivesButton2.append(additivesCircle2, additivesCin);
+  additivesButton3.append(additivesCircle3, additivesSy);
+  additives.append(additivesTitle, additivesButtons);
+
+  sizeButton1.append(sizeCircle1, sizeSml);
+  sizeButton2.append(sizeCircle2, sizeMml);
+  sizeButton3.append(sizeCircle3, sizeLml);
+  sizeButtons.append(sizeButton1, sizeButton2, sizeButton3);
+  sizeBlock.append(sizeTitle, sizeButtons);
+  titleBlock.append(title, subTitle);
+
+  totalBlock.append(totalText, totalPrice);
+
+  alertBlock.append(alertSvg, alertTitle);
+
+
+  blockText.append(titleBlock, sizeBlock, additives, totalBlock, alertBlock, closeButton);
+  modal.append(blockImage, blockText);
+  blockImage.append(modalImage);
+  manuList.append(shadow, modal);  
+}
+
 
 tabsReload.append(svgReload);
 tabsLink1Iconblock.append(tabsLink1Icon);
