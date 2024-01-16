964f498d64264e8546dc5d5fcbdabbae5f283f75
anthony-th <yefremov.anthony@gmail.com>
2024-01-16 21:10:46 +0300

    feat: add transitionend for modal (Tue, Jan 16, 2024 09:11:59 PM)

--- hangman/src/components/keyboard.js
+++ hangman/src/components/keyboard.js
@@ -5,10 +5,12 @@ import { titleModal, modalAnswer, modal } from "./modal";
 import dataJson from '../data/questions.json';
 
 const maxFails = 6;
+let randomWord;
 let currentFails = 0;
 let listItems = [];
 let gameOver = false;
 let wasQuestions = [];
+let isReloadPage = true;
 
 const functionBlock = createElement('div', 'section-logic');
 const maskAnswer = createElement('ul', 'list');
@@ -16,10 +18,8 @@ const question = createElement('h2', 'title-question');
 const fails = createElement('div', 'fails');
 const failsTitle = createElement('h3', 'subtitle')
 failsTitle.textContent = 'Fails: ';
-
 const greenSpan = createElement('span', 'green');
 greenSpan.textContent = currentFails;
-
 const orangeSpan = createElement('span', 'orange');
 orangeSpan.textContent = maxFails;
 fails.append(failsTitle, greenSpan, `/`, orangeSpan);
@@ -30,6 +30,11 @@ function generateMaskBlock(letter) {
   item.dataset.letter = letter;
   maskAnswer.append(item);
   listItems.push(item);
+}
+
+function endTranslateY(answer) {
+  modalAnswer.textContent = `Corrent answer: ${answer}`;
+  modal.removeEventListener('transitionend', endTranslateY);
 }
 
 export function playAgain() {
@@ -45,7 +50,6 @@ export function playAgain() {
   greenSpan.textContent = currentFails;
   getRandomQuestion();
   gameOver = false;
-
   for (let charCode = 65; charCode <= 90; charCode++) {
     const button = document.getElementById(`key-id-${charCode - 64}`);
     button.disabled = false;
@@ -60,7 +64,7 @@ function getRandomQuestion() {
     console.log(`This game has a total of ${dataJson.length} questions. You have answered all ${dataJson.length}   questions. From now on, the questions will start repeating.`);
   }
   const randomIndex = Math.floor(Math.random() * unusedQuestion.length);
-  const randomWord = unusedQuestion[randomIndex];
+  randomWord = unusedQuestion[randomIndex];
   wasQuestions.push(randomWord.id);
   question.textContent = randomWord.question;
   maskAnswer.innerHTML = '';
@@ -68,9 +72,12 @@ function getRandomQuestion() {
   listItems = [];
   const letters = randomWord.answer.split('');
   letters.forEach(letter => generateMaskBlock(letter));
-  modalAnswer.textContent = `Corrent answer: ${randomWord.answer}`;
-  console.log(`Answer: ` + randomWord.answer  );
-  console.log('Please switch to English keyboard layout!');
+  if (isReloadPage) {
+    modalAnswer.textContent = `Corrent answer: ${randomWord.answer}`;
+    console.log('Please, don\'t forget to switch to the English keyboard layout.');
+    isReloadPage = false;
+  }
+  console.log(`Answer: ` + randomWord.answer);
   return randomWord;
 }
 
@@ -78,30 +85,23 @@ functionBlock.append(maskAnswer, question, fails);
 
 export const createKeyboard = function() {
   const keyboard = createElement('div', 'keyboard-wrapper');
-
   for (let charCode = 65; charCode <= 90; charCode++) {
     const letter = String.fromCharCode(charCode);
     const button = createElement("button", "button");
     button.textContent = letter;
     button.id = `key-id-${charCode - 64}`;
-
     keyboard.append(button);
     button.onclick = () => buttonPress(letter, button);
   }
-
   functionBlock.append(keyboard);
   return functionBlock;
 }
-
-getRandomQuestion();
 
 function buttonPress(letter, button) {
   if (gameOver || button.disabled) {
     return;
   }
-
   const filterItems = listItems.filter(item => item.dataset.letter.toLowerCase() === letter.toLowerCase());
-
   if (filterItems.length > 0) {
     filterItems.forEach(item => {
       item.textContent = letter;
@@ -139,9 +139,12 @@ function buttonPress(letter, button) {
 }
 
 document.onkeydown = (event) => pressDownKeyboard(event);
+modal.addEventListener('transitionend', () => endTranslateY(randomWord.answer))
 
 function pressDownKeyboard(event) {
-  if (event.keyCode >= 65 && event.keyCode <= 90) {
+  if (event.keyCode === 13 && shadow.style.display === 'block') {
+    playAgain();
+  } else if (event.keyCode >= 65 && event.keyCode <= 90) {
     const letter = String.fromCharCode(event.keyCode);
     const button = document.getElementById(`key-id-${event.keyCode - 64}`);
     buttonPress(letter, button);
@@ -172,3 +175,5 @@ function updateVisibility() {
       break;
   }
 }
+
+getRandomQuestion();
