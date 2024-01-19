1eaa071b3bd7d2aebda29e692a1f574c7f591c59
anthony-th <yefremov.anthony@gmail.com>
2024-01-19 03:25:02 +0300

    refactor: change title egg (Fri, Jan 19, 2024 03:25:01 AM)

--- hangman/src/components/header.js
+++ hangman/src/components/header.js
@@ -3,7 +3,7 @@ import { createElement } from "./createElement"
 export const header = createElement('header', 'header');
 const title = createElement('h1', 'title');
 export const subtitle = createElement('h2', 'subtitle');
-subtitle.textContent = 'wo';
+subtitle.textContent = 'Cyrus';
 title.textContent = 'hangman';
 export const headerImage = createElement('img', 'header-image');
 headerImage.src = './assets/img/wood.webp';
