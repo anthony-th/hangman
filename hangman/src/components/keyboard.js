f84eb3097b2aa57ed3b2451281b3136c0c6006a4
anthony-th <yefremov.anthony@gmail.com>
2024-01-20 03:22:47 +0300

    refactor: optimization visualization code (Sat, Jan 20, 2024 03:22:47 AM)

--- hangman/src/components/keyboard.js
+++ hangman/src/components/keyboard.js
@@ -1,5 +1,5 @@
 import { createElement } from "./createElement";
-import { man1, man2, man3, man4, man5, man6, woman1, woman2, woman3, woman4, woman5, woman6, woman7, imagesWrapper, gallows } from "./visualization";
+import { manImages, woman1, womanImages, imagesWrapper, gallows } from "./visualization";
 import { shadow } from "..";
 import { titleModal, modalAnswer, modal } from "./modal";
 import dataJson from '../data/questions.json';
@@ -42,19 +42,13 @@ function endTranslateY(answer) {
 
 export function playAgain() {
   if (imagesWrapper.contains(gallows)) {
-    man1.style.visibility = 'hidden';
-    man2.style.visibility = 'hidden';
-    man3.style.visibility = 'hidden';
-    man4.style.visibility = 'hidden';
-    man5.style.visibility = 'hidden';
-    man6.style.visibility = 'hidden';
+    manImages.forEach(manImg => {
+      manImg.style.visibility = 'hidden';
+    });
     woman1.style.visibility = 'hidden';
-    woman2.style.visibility = 'hidden';
-    woman3.style.visibility = 'hidden';
-    woman4.style.visibility = 'hidden';
-    woman5.style.visibility = 'hidden';
-    woman6.style.visibility = 'hidden';
-    woman7.style.visibility = 'hidden';
+    womanImages.forEach(womanImg => {
+      womanImg.style.visibility = 'hidden';
+    });
     shadow.style.display = 'none';
     modal.style.transform = 'translateY(-100vh)';
     currentFails = 0;
@@ -67,18 +61,12 @@ export function playAgain() {
     }
   } else {
     woman1.style.visibility = 'visible';
-    woman2.style.visibility = 'hidden';
-    woman3.style.visibility = 'hidden';
-    woman4.style.visibility = 'hidden';
-    woman5.style.visibility = 'hidden';
-    woman6.style.visibility = 'hidden';
-    woman7.style.visibility = 'hidden';
-    man1.style.visibility = 'hidden';
-    man2.style.visibility = 'hidden';
-    man3.style.visibility = 'hidden';
-    man4.style.visibility = 'hidden';
-    man5.style.visibility = 'hidden';
-    man6.style.visibility = 'hidden';
+    womanImages.forEach(womanImg => {
+      womanImg.style.visibility = 'hidden';
+    });
+    manImages.forEach(manImg => {
+      manImg.style.visibility = 'hidden';
+    });
     shadow.style.display = 'none';
     modal.style.transform = 'translateY(-100vh)';
     currentFails = 0;
@@ -195,28 +183,28 @@ function pressDownKeyboard(event) {
 function updateVisibility() {
   switch (currentFails) {
     case 1:
-      man1.style.visibility = 'visible';
-      woman2.style.visibility = 'visible';
+      manImages[0].style.visibility = 'visible';
+      womanImages[0].style.visibility = 'visible';
       break;
     case 2:
-      man2.style.visibility = 'visible';
-      woman3.style.visibility = 'visible';
+      manImages[1].style.visibility = 'visible';
+      womanImages[1].style.visibility = 'visible';
       break;
     case 3:
-      man3.style.visibility = 'visible';
-      woman4.style.visibility = 'visible';
+      manImages[2].style.visibility = 'visible';
+      womanImages[2].style.visibility = 'visible';
       break;
     case 4:
-      man4.style.visibility = 'visible';
-      woman5.style.visibility = 'visible';
+      manImages[3].style.visibility = 'visible';
+      womanImages[3].style.visibility = 'visible';
       break;
     case 5:
-      man5.style.visibility = 'visible';
-      woman6.style.visibility = 'visible';
+      manImages[4].style.visibility = 'visible';
+      womanImages[4].style.visibility = 'visible';
       break;
     case 6:
-      man6.style.visibility = 'visible';
-      woman7.style.visibility = 'visible';
+      manImages[5].style.visibility = 'visible';
+      womanImages[5].style.visibility = 'visible';
       break;
     default:
       break;
--- hangman/src/components/visualization.js
+++ hangman/src/components/visualization.js
@@ -2,75 +2,50 @@ import { createElement } from "./createElement";
 import { headerImage, subtitle } from "./header";
 
 export const imagesWrapper = createElement('div', 'section-gallows');
+
 export const manWrapper = createElement('div', 'gallows-man');
-export const gallows = createElement('img', 'gallows');
-gallows.src = './assets/img/1.webp';
-gallows.alt = '';
-export const man1 = createElement('img', 'img');
-man1.src = './assets/img/2.webp';
-man1.alt = '';
-export const man2 = createElement('img', 'img');
-man2.src = './assets/img/3.webp';
-man2.alt = '';
-export const man3 = createElement('img', 'img');
-man3.src = './assets/img/4.webp';
-man3.alt = '';
-export const man4 = createElement('img', 'img');
-man4.src = './assets/img/5.webp';
-man4.alt = '';
-export const man5 = createElement('img', 'img');
-man5.src = './assets/img/6.webp';
-man5.alt = '';
-export const man6 = createElement('img', 'img');
-man6.src = './assets/img/7.webp';
-man6.alt = '';
+export const gallows = createImage('./assets/img/1.webp', 'gallows');
+export const manImages = [
+  createImage('./assets/img/2.webp', 'img'),
+  createImage('./assets/img/3.webp', 'img'),
+  createImage('./assets/img/4.webp', 'img'),
+  createImage('./assets/img/5.webp', 'img'),
+  createImage('./assets/img/6.webp', 'img'),
+  createImage('./assets/img/7.webp', 'img'),
+];
 
 export const womanWrapper = createElement('div', 'gallows-woman');
-const womanGallows = createElement('img', 'woman-gallows');
-womanGallows.src = './assets/img/c1.webp';
-womanGallows.alt = '';
-export const woman1 = createElement('img', 'woman-img');
-woman1.src = './assets/img/c2.webp';
-woman1.alt = '';
-export const woman2 = createElement('img', 'woman-img');
-woman2.src = './assets/img/c3.webp';
-woman2.alt = '';
-export const woman3 = createElement('img', 'woman-img');
-woman3.src = './assets/img/c4.webp';
-woman3.alt = '';
-export const woman4 = createElement('img', 'woman-img');
-woman4.src = './assets/img/c5.webp';
-woman4.alt = '';
-export const woman5 = createElement('img', 'woman-img');
-woman5.src = './assets/img/c6.webp';
-woman5.alt = '';
-export const woman6 = createElement('img', 'woman-img');
-woman6.src = './assets/img/c7.webp';
-woman6.alt = '';
-export const woman7 = createElement('img', 'woman-img');
-woman7.src = './assets/img/c8.webp';
-woman7.alt = '';
+export const womanGallows = createImage('./assets/img/c1.webp', 'woman-gallows');
+export const woman1 = createImage('./assets/img/c2.webp', 'woman-img');
+export const womanImages = [
+  createImage('./assets/img/c3.webp', 'woman-img'),
+  createImage('./assets/img/c4.webp', 'woman-img'),
+  createImage('./assets/img/c5.webp', 'woman-img'),
+  createImage('./assets/img/c6.webp', 'woman-img'),
+  createImage('./assets/img/c7.webp', 'woman-img'),
+  createImage('./assets/img/c8.webp', 'woman-img'),
+];
 
-manWrapper.append(man1, man2, man3, man4, man5, man6);
-womanWrapper.append(woman1, woman2, woman3, woman4, woman5, woman6, woman7);
+function createImage(src, alt) {
+  const img = createElement('img', alt);
+  img.src = src;
+  img.alt = '';
+  return img;
+}
+
+manWrapper.append(...manImages);
+womanWrapper.append(woman1, ...womanImages);
 imagesWrapper.append(gallows, manWrapper);
 
+//Egg
 headerImage.onclick = toggleImages;
 
 function toggleImages() {
-  if (imagesWrapper.contains(gallows)) {
-    imagesWrapper.removeChild(gallows);
-    imagesWrapper.removeChild(manWrapper);
-    imagesWrapper.appendChild(womanWrapper);
-    imagesWrapper.appendChild(womanGallows);
-    woman1.style.visibility = 'visible';
-    subtitle.style.visibility = 'visible';
-  } else {
-    woman1.style.visibility = 'hidden';
-    subtitle.style.visibility = 'hidden';
-    imagesWrapper.removeChild(womanGallows);
-    imagesWrapper.removeChild(womanWrapper);
-    imagesWrapper.appendChild(manWrapper);
-    imagesWrapper.appendChild(gallows);
-  }
+  const imageContains = imagesWrapper.contains(gallows);
+  imagesWrapper.removeChild(imageContains ? gallows : womanGallows);
+  imagesWrapper.removeChild(imageContains ? manWrapper : womanWrapper);
+  imagesWrapper.appendChild(imageContains ? womanWrapper : manWrapper);
+  imagesWrapper.appendChild(imageContains ? womanGallows : gallows);
+  woman1.style.visibility = imageContains ? 'visible' : 'hidden';
+  subtitle.style.visibility = imageContains ? 'visible' : 'hidden';
 }
