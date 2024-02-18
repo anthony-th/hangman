33571f7176d2be2398b8c21cfb076df63c88f166
anthony-th <yefremov.anthony@gmail.com>
2024-02-18 09:46:35 +0300

    refactor: add rel for footer-link (Sun, Feb 18, 2024 09:46:34 AM)

--- hangman/src/components/footer.js
+++ hangman/src/components/footer.js
@@ -5,6 +5,7 @@ const copyrightText = createElement('p', 'copyright-text');
 copyrightText.textContent = 'Copyright';
 const linkAuthor = createElement('a', 'copyright-link');
 linkAuthor.href = 'https://github.com/anthony-th';
+linkAuthor.rel = 'noreferrer noopener';
 linkAuthor.target = '_blank';
 linkAuthor.textContent = 'anthony-th';
 const copyrightYeah = createElement('p', 'copyright-text');
