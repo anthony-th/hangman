41cb9891f90a6dbe35473d180bdbe06755f5d293
anthony-th <yefremov.anthony@gmail.com>
2024-08-10 18:21:41 +0300

    refactor: upd createElement for visualization.js (Sat, Aug 10, 2024 06:21:41 PM)

--- src/components/visualization.js
+++ src/components/visualization.js
@@ -2,9 +2,7 @@ import { createElement } from '../common/createElement';
 import { headerImage, subtitle } from './header';
 
 const createImage = (src, alt) => {
-  const img = createElement('img', alt);
-  img.src = src;
-  img.alt = '';
+  const img = createElement('img', alt, '', { src, alt: '' });
   return img;
 };
 
@@ -32,8 +30,7 @@ const womanImages = [
   createImage('./assets/img/c8.webp', 'woman-img'),
 ];
 
-const newGame = createElement('button', 'text');
-newGame.textContent = 'new game?';
+const newGame = createElement('button', 'text', 'new game?');
 
 manWrapper.append(...manImages);
 womanWrapper.append(woman1, ...womanImages);
