a689a80ba5d2de623a9f7ccf8b5f571cc90f0b90
anthony-th <yefremov.anthony@gmail.com>
2024-08-10 18:16:03 +0300

    refactor: upd createElement for modal.js (Sat, Aug 10, 2024 06:16:02 PM)

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,15 +1,19 @@
 import { createElement } from '../common/createElement';
 
 const modal = createElement('div', 'modal-wrapper');
-const titleModal = createElement('h2', 'title-modal');
-titleModal.textContent = 'You win';
+const titleModal = createElement('h2', 'title-modal', 'You win');
 const modalAnswerBlock = createElement('div', 'modal-answer');
-const modalAnswerSubtitle = createElement('h3', 'modal-subtitle');
-modalAnswerSubtitle.textContent = `Correct answer:`;
-const modalAnswerText = createElement('p', 'modal-text');
-modalAnswerText.textContent = '';
-const buttonTryAgain = createElement('button', 'button-try-again');
-buttonTryAgain.textContent = 'play again!';
+const modalAnswerSubtitle = createElement(
+  'h3',
+  'modal-subtitle',
+  'Correct answer:'
+);
+const modalAnswerText = createElement('p', 'modal-text', '');
+const buttonTryAgain = createElement(
+  'button',
+  'button-try-again',
+  'play again!'
+);
 
 modalAnswerBlock.append(modalAnswerSubtitle, modalAnswerText);
 modal.append(titleModal, modalAnswerBlock, buttonTryAgain);
