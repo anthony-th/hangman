05c6f0ac8053b5083b1c9b2a1632bd0673295089
anthony-th <yefremov.anthony@gmail.com>
2024-08-16 21:15:46 +0300

    refactor: change images names (Fri, Aug 16, 2024 09:15:46 PM)

--- src/components/visualization.js
+++ src/components/visualization.js
@@ -8,11 +8,14 @@ const createImage = (src, alt) => {
 
 const imagesWrapper = createElement('div', 'section-gallows');
 const manWrapper = createElement('div', 'gallows-man');
-const gallows = createImage('./assets/img/man/1.webp', 'gallows');
+const gallows = createImage('./assets/img/man/gallows.webp', 'gallows');
 const manImages = createImage('./assets/img/man/man1.webp', 'img');
 
 const womanWrapper = createElement('div', 'gallows-woman');
-const womanGallows = createImage('./assets/img/syrus/c1.webp', 'woman-gallows');
+const womanGallows = createImage(
+  './assets/img/syrus/beam.webp',
+  'woman-gallows'
+);
 const woman1 = createImage('./assets/img/syrus/ball.webp', 'woman-img');
 const womanImages = createImage('./assets/img/syrus/syrus1.webp', 'woman-img');
 
